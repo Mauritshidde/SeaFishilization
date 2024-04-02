53a7609246140c28dbf7194443f9bd9053b54598
Mauritshidde <Mauritshidde@gmail.com>
2024-04-02 18:44:18 +0200

    main menu improvements
    main menu ongeveer af

--- src/main.cpp
+++ src/main.cpp
@@ -3,57 +3,57 @@
 #include "raylib.h"
 
 #include "ui/menu.h"
+#include "ui/settings.h"
 
 int screenWidth = 0;
 int screenHeight = 0;
 
-void Draw2DGrid()
-{
-    DrawLine(0, 0, screenWidth, 0, BLACK);
-}
-
-void Start()
-{
-}
-
-void Update()
-{
-}
-
-void Render()
-{
-    BeginDrawing();
-    ClearBackground(WHITE);
-    Draw2DGrid();
-    EndDrawing();
-}
-
 int main()
 {
+    bool quitGame = false;
+
     InitWindow(0, 0, "airplane simulation");
-    ToggleFullscreen();
     SetTargetFPS(60);
     Menu menu;
+    Settings settingsMenu;
 
     screenWidth = GetScreenWidth();
     screenHeight = GetScreenHeight();
     menu = Menu(screenWidth, screenHeight);
+    settingsMenu = Settings(screenWidth, screenHeight);
 
-    while (menu.startScreen)
-    {
-        menu.Update(screenWidth, screenHeight);
-        menu.Draw(screenWidth, screenHeight);
-    }
+    while (!quitGame) // dit is de gameloop voor het mainMenu deze zolang deze acttief is runt de game
+    {       
+        while (menu.startScreen) // deze loop gaat door tot je een keuze hebt gemaakt in het main menu
+        {
+            menu.Update(screenWidth, screenHeight);
+            menu.Draw(screenWidth, screenHeight);
+        }
+        menu.startScreen = true; // set menu screen back to true so that when you quit to main menu you gt back to the main menu screen
 
-    if (menu.buttonPressed == 1) {
-        std::cout << "ja" << std::endl;
-    }
-
-    while (!WindowShouldClose())
-    {
-        Start();
-        Update();
-        Render();
+        if (menu.buttonPressed == 0) // optie 1 start game
+        {
+            // dit moet worden vervangen met een functie of class die in een andere file moet
+            // while (!WindowShouldClose())
+            // {
+            //     Start();
+            //     Update();
+            //     Render();
+            // }
+        }
+        else if (menu.buttonPressed == 1) // optie 2 settings
+        {
+            while (settingsMenu.startScreen)
+            {
+                settingsMenu.Update(screenWidth, screenHeight);
+                settingsMenu.Draw(screenWidth, screenHeight);
+            }
+            settingsMenu.startScreen = true; // same reason as for main menu, and declaration not in this if becuase this is better for response time, but worse for start up time
+        }
+        else // optie 3 quit game
+        {
+            quitGame = true;
+        }
     }
 
     return 0;
