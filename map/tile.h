95605e9dafef302982a7a1a8db26c612a1166d56
Twan Reijinga <43951307+Twan-Reijinga@users.noreply.github.com>
2024-04-03 15:25:03 +0200

    add temp draw and init func to tile with tilesize and pos

--- map/tile.h
+++ map/tile.h
@@ -2,12 +2,24 @@ class Tile {
     private: 
         /* data */
     public:
-        Tile(/* args */);
+        Vector2 pos;
+        int tileSize;
+        int type;
+        Tile(int x, int y, int size);
+        int Draw();
         ~Tile();
 };
 
-Tile::Tile(/* args */) {
+Tile::Tile(int x, int y, int size = 30) {
+    pos.x = x;
+    pos.y = y;
+    tileSize = size;
+}
 
+int Tile::Draw() {
+    Color tileColor = {50, 0, 100, 255}; // temp
+    DrawRectangle(pos.x * tileSize, pos.y * tileSize, tileSize, tileSize, tileColor);
+    return 0;
 }
 
 Tile::~Tile() {
